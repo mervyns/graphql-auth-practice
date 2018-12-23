c521e65438b3cb134d2eb70b751b5ddf00216da9
mervyns <mervyns@gmail.com>
2018-12-23 18:16:39 +0800

    Added mutation to loginform

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -7,20 +7,26 @@ class AuthForm extends Component {
     this.state={ email : '', password: ''};
   }
   
+  onSubmit(event) {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state);
+  }
+
   render() {
     return (
       <div className="row">
-      <form className="col s4">
+      <form onSubmit={this.onSubmit.bind(this)} className="col s4">
         <div className="input-field">
-          <label>Email</label>
-          <input 
+          <input
+            placeholder="Email"
             value={this.state.email}
             onChange={e => this.setState({ email: e.target.value })}
           />
         </div>
         <div className="input-field">
-          <label>Password</label>
           <input
+            placeholder="Password"
             value={this.state.password}
             onChange={e => this.setState({ password: e.target.value })}
           />
